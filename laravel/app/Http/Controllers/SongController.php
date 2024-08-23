b2cac358fc22f962027100ec8acd4e2539998a7c
KatarinaMilosevic1 <kaca.milosevic001@gmail.com>
2024-08-23 22:06:38 +0200

    kreiran kontroler i api rute za pesme

---
+++ laravel/app/Http/Controllers/SongController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Song;
+use App\Models\Band;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SongController extends Controller
+{
+    /**
+     * Prikazuje listu svih pesama.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $songs = Song::with('band')->get();
+        return response()->json([
+            'success' => true,
+            'data' => $songs
+        ], 200);
+    }
+
+    /**
+     * Prikazuje pojedinačnu pesmu po ID-u.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $song = Song::with('band')->findOrFail($id);
+        return response()->json([
+            'success' => true,
+            'data' => $song
+        ], 200);
+    }
+
+    /**
+     * Kreira novu pesmu.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'band_id' => 'required|exists:bands,id',
+            'title' => 'required|string|max:255',
+            'youtube_url' => 'nullable|url',
+            'spotify_url' => 'nullable|url',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $song = Song::create($validator->validated());
+
+        return response()->json([
+            'success' => true,
+            'data' => $song
+        ], 201);
+    }
+
+    /**
+     * Ažurira postojeću pesmu.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $song = Song::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'band_id' => 'required|exists:bands,id',
+            'title' => 'required|string|max:255',
+            'youtube_url' => 'nullable|url',
+            'spotify_url' => 'nullable|url',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $song->update($validator->validated());
+
+        return response()->json([
+            'success' => true,
+            'data' => $song
+        ], 200);
+    }
+
+    /**
+     * Briše pesmu iz baze podataka.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $song = Song::findOrFail($id);
+        $song->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Song deleted successfully'
+        ], 200);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BandController;
+use App\Http\Controllers\SongController;
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -15,6 +17,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bands', [BandController::class, 'store']);
     Route::put('/bands/{id}', [BandController::class, 'update']);
     Route::delete('/bands/{id}', [BandController::class, 'destroy']);
+
+
+    Route::get('/songs', [SongController::class, 'index']);
+    Route::get('/songs/{id}', [SongController::class, 'show']);
+    Route::post('/songs', [SongController::class, 'store']);
+    Route::put('/songs/{id}', [SongController::class, 'update']);
+    Route::delete('/songs/{id}', [SongController::class, 'destroy']);
 });
 
 
